ffbdd147d60d959d1b865b0bd308f749a7455bb1
Kevin Shin <kevshin2002@gmail.com>
2023-07-27 11:53:30 -0700

    Scripts.

---
+++ Testing/ScriptEngine.hpp
@@ -0,0 +1,29 @@
+//
+//  ScriptEngine.hpp
+//  
+//
+//  Extension of Relational Database, ECE 141B
+//  Copyright UCSD ECE 141B, Prof Rick Gessner
+//  Coded by Kevin Shin
+//
+
+#ifndef ScriptEngine_hpp
+#define ScriptEngine_hpp
+
+/*
+ScriptEngine
+Reads scripts
+returns a string
+Get filepath
+
+need comparator
+
+
+*/
+namespace ECE141 {
+	class ScriptEngine {
+	public:
+	};
+}
+
+#endif // ScriptEngine.hpp
